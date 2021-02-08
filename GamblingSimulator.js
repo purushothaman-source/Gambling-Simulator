18d13326d811df0f1baf6c35d9fbb27569abb7d0
purushothaman-source <purushoth99jun@gmail.com>
2021-02-08 09:04:35 +0530

    ADDED : make 1 rs bet

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -1,4 +1,11 @@
 console.log("Welcome to Gambilng Simulator");
 var totalAmount = 100;
 var betPerGame = 1;
-  
+var result = Math.floor(Math.random * 2);
+var win = 1;
+var lose = 2;
+if (result == win){
+    totalAmount++;
+}else{
+    totalAmount--;
+}  
