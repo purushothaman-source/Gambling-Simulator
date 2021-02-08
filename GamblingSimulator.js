f02461248751a767e201c995a1da64c187e373af
purushothaman-source <purushoth99jun@gmail.com>
2021-02-08 09:38:45 +0530

    ADDED : code to resign game

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -1,11 +1,14 @@
 console.log("Welcome to Gambilng Simulator");
 var totalAmount = 100;
 var betPerGame = 1;
-var result = Math.floor(Math.random * 2);
 var win = 1;
 var lose = 2;
-if (result == win){
-    totalAmount++;
-}else{
-    totalAmount--;
-}  
+while (totalAmount != 150 && totalAmount != 50) {
+    var result = Math.floor(Math.random() * 10)%2;
+    if(result == win) {
+        totalAmount++;
+    } else {
+        totalAmount--;
+    }
+}
+console.log(totalAmount);
